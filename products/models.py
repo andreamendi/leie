941c1146dbf1d52ae92eaf916969f1388798819d
AndreaMendi <andreamendi@icloud.com>
2018-11-28 22:13:47 -0600

    Ya estan creadas las tablas y sus relaciones, solo falta ranking-user

--- products/models.py
+++ products/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+	name = models.CharField(max_length = 100, null = False)
+	create_at = models.DateTimeField(auto_now_add=True) # Here we've the date when the user will create the product
+	modified_at = models.DateTimeField(auto_now=True) # Here we've the date when the user modify the product
+
+	def __str__(self):
+		return 'Name: {}'.format(self.name)
+
 
 class Product(models.Model):
 	name = models.CharField(max_length = 60, null = False)
@@ -11,28 +21,40 @@ class Product(models.Model):
 	taken = models.BooleanField(null = False)
 	price = models.FloatField(default=0.01)
 	comments = models.TextField(max_length = 180, null = False)
-	# ranking_product = models.ManyToManyField(User, through='RankingProduct') Ver donde pongo esto, ya que no está el user
-	create_at = models.DateTimeField(auto_now_add=True) #Here we've the date when the user will create the product
-	modified_at = models.DateTimeField(auto_now=True) #Here we've the date when the user modify the product
+	user = models.ForeignKey(User, on_delete=models.CASCADE) #La FK se pone en la que no es la "dueña"
+	rents = models.ManyToManyField(User, through='Rent', related_name='rents') # Tu tienes una relación con User ManyToMany
+	categories = models.ManyToManyField(Category)
+	create_at = models.DateTimeField(auto_now_add=True) # Here we've the date when the user will create the product
+	modified_at = models.DateTimeField(auto_now=True) # Here we've the date when the user modify the product
 
-class Category(models.Model):
-	name = models.CharField(max_length = 100, null = False)
-	create_at = models.DateTimeField(auto_now_add=True) #Here we've the date when the user will create the product
-	modified_at = models.DateTimeField(auto_now=True) #Here we've the date when the user modify the product
+	def __str__(self):
+		return 'Name: {}, user: {}'.format(self.name,self.user)
+
+
+
+
 
 class Card(models.Model):
 	card_number = models.CharField(max_length = 50, null = False)
 	secure_number = models.CharField(max_length = 4, null = False)
 	valid_thru = models.CharField(max_length = 4, null = False)
-	create_at = models.DateTimeField(auto_now_add=True) #Here we've the date when the user will create the product
-	modified_at = models.DateTimeField(auto_now=True) #Here we've the date when the user modify the product
-	# user = models.ForeignKey(Reporter)
+	full_name = models.CharField(max_length = 100, null = False)
+	create_at = models.DateTimeField(auto_now_add=True) # Here we've the date when the user will create the product
+	modified_at = models.DateTimeField(auto_now=True) # Here we've the date when the user modify the product
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-class RankingProduct(models.Model):
-  # user = models.ForeignKey(User)
-  # products = models.ForeignKey(Products)
+	def __str__(self):
+		return 'Fullname: {}, user: {}'.format(self.full_name,self.user)
+
+
+class Rent(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
 	score = models.IntegerField(default = 0)
 	begin_day =  models.TimeField(auto_now = False,null = False)
 	end_day = models.TimeField(auto_now = False,null = False)
 	create_at = models.DateTimeField(auto_now_add=True) #Here we've the date when the user will create the product
 	modified_at = models.DateTimeField(auto_now=True) #Here we've the date when the user modify the product
+	
+	def __str__(self):
+		return 'User: {}, Product: {}'.format(self.user,self.product)
